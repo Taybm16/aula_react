61c66a5914db43d07b4fdd091e1ae533fa4e8171
Tayna <taynabm16@hotmail.com>
2025-09-16 13:47:56 -0300

    primeiro commit

---
+++ aula_react/src/App.jsx
@@ -0,0 +1,14 @@
+import gojo from "./gojo.webp"
+import './App.css';
+
+function App() {
+  return (
+   <>
+    <img src= {gojo} alt="Satoru Gojo"/>
+    
+  
+   </>
+  );
+}
+
+export default App;
