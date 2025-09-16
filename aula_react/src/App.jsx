36d35241f7e81aaeb8e3e7d78ed33bc892d8f8c3
Tayna <taynabm16@hotmail.com>
2025-09-16 14:21:11 -0300

    Iniciando curriculo

--- aula_react/src/App.jsx
+++ aula_react/src/App.jsx
@@ -1,13 +1,63 @@
-import gojo from "./gojo.webp"
+
 import './App.css';
 
 function App() {
   return (
-   <>
-    <img src= {gojo} alt="Satoru Gojo"/>
+    <>
+      <h1>Tayná Bethoud Marques</h1>
+      <p>✨Sobre Mim: Estudante de Engenharia de Software, atuo com foco em desenvolvimento Front-end.
+      </p>
+      <br/>
+      <p>💻 Minha stack atual inclui: HTML5, CSS3, JavaScript, Python</p>
+      <br/>
+      <h2>Experiências práticas:</h2>
+      <ul>
+        <li>Freelances com desenvolvimento de landing pages responsivas</li>
+        <li>Instrutora em cursos de programação, auxiliando no aprendizado de iniciantes</li>
+        <li>Em constante evolução, curso Análise e Desenvolvimento de Sistemas e Engenharia de Software, além de formações complementares em desenvolvimento web.</li>
+      </ul>
+      <p>✨ Apaixonada por tecnologia, sou movida pela curiosidade e desafios. Tenho como principais características a organização, confiabilidade e colaboração em equipes ágeis.</p>
     
-  
-   </>
+      <h2>🌍Idiomas</h2>
+      <ul>
+        <li>Inglês: Proficiência que me permite colaborar em projetos internacionais.</li>
+      </ul>
+      <p>✨ Estou empenhada em contribuir com soluções inovadoras e agregar valor às equipes no cenário tecnológico. Vamos criar juntos um futuro ainda mais promissor? 🚀</p>
+      <p>📧 E-mail: taynabm16@hotmail.com</p>
+      <a href=" https://github.com/Taybm16">🔗 GitHub</a>
+      <br />
+      <a href="https://www.linkedin.com/in/taynaberthoud/">Linkedin</a> 
+
+      <h2>Experiências:</h2>
+      <h2>INFINITY SCHOOL - Visual Art Creative Center</h2>
+    <p>Instrutora</p>
+    <p>ago de 2025 - o momento · 2 meses</p>
+    <p>São Paulo, Brasil · Presencial</p>
+    <p>Monitora | Programação</p>
+    <p>mai de 2025 - o momento · 5 meses</p>
+    <p>Meio período</p>
+    <p>Competências: JavaScript · Comunicação · Confidencialidade · Python · Atenção a detalhes · Treinamento · Habilidades analíticas</p>
+
+    <h2>LabWare</h2>
+    <p>Estagiária - LCCP</p>
+    <p>jan de 2025 - mai de 2025 · 5 meses</p>
+    <p>São Paulo, Brasil · Híbrida</p>
+    <p>Competências: Confidencialidade · Atenção a detalhes · Habilidades analíticas</p>
+
+    <h2>MFX Academy Technology</h2>
+    <p>Monitora | Desenvolvimento Front End</p>
+    <p>set de 2024 - abr de 2025 · 8 meses</p>
+    <p>São Paulo, Brasil · Presencial</p>
+    <p>Competências: Confidencialidade · Atenção a detalhes · Treinamento · Habilidades analíticas</p>
+
+    <h2>Wilu</h2>
+    <p>Desenvolvedora | Estagiária</p>
+    <p>ago de 2024 - dez de 2024 · 5 meses</p>
+    <p>São Paulo, Brasil · Remota</p>
+    <p>Competências: Confidencialidade · Gestão de projetos · Programação (computação) · Suporte administrativo · Documentos jurídicos · Atenção a detalhes · Operações jurídicas · Comunicação interpessoal · Habilidades analíticas</p>
+
+
+    </>
   );
 }
 
